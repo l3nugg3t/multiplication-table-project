8d6724fe5d70f9ef9bf4834365a3d8d537dc11ea
Le Nguyen <nguy3903@umn.edu>
2025-10-13 15:11:05 -0500

    Corrected index html in button and JS (else)

--- javascript.js
+++ javascript.js
@@ -1,67 +1,15 @@
-console.log('Here\'s a for loop showing 1...10:\nn')
-for (let i = 0; i < 10; i++) {
-    console.log(`${i}\n`)
-}
-
-
-console.log('Here\'s a do-while loop showing 1...10:\n')
-let i = 0
-do {
-    console.log(i + '\n')
-    i++
-} while (i < 10)
-
-
-console.log('Here\'s a do-while loop showing 1...10:\n')
-let reagan = 0
-while (reagan < 10) {
-    console.log(`${reagan}\n`)
-    reagan++
-}
-
-console.log('Here\'s a foreach loop iterating over an array showing 1...1000:\n')
-let nums = [...Array(10).keys()]
-nums.forEach(num => {
-    console.log(`${num}\n`)
-})
-
-console.log('Here\'s a multiplication table method rendered in the console with a for loop:\n')
-let timesTable = (to) => {
-    console.log(`Here\'s a times tables to ' + to \n`)
-
-    if (Number.isInteger(to)) {
-        for (let i = 1; i <= to; i++) {
-            let rowContents = ""
-            for (let col = 1; col <= to; col++) {
-                rowContents += `${i * col}\t`
-            }
-            console.log(rowContents)
-        }
-    } else {
-        console.log(`Sorry, ${to} isn't a number`)
-    }
-}
-
-function getRowsDesired() {
-    return Number(document.getElementById("rows").value);
-}
-
-function getColumnsDesired() {
-    return Number(document.getElementById("cols").value);
-}
-
-function getHighlightValue() {
-    let highlightValue = document.getElementById("highlight").value;
-
-    if (highlightValue !== "") {
-        return Number(highlightValue)
-    }
-}
 
 // this method can be used to replace the 3 duplicates above = less code
-function getInputValue(inputId) {
-    return Number(document.getElementById(inputId).value);
-
+function getInputValueAsNumber(inputId) {
+    if (document.getElementById(inputId)) {
+        if (document.getElementById(inputId).value !== null) {
+            return Number(document.getElementById(inputId).value);
+        } else {
+            console.log("sorry, value isn't present for this ID: ", inputId);
+        }
+    } else {
+        console.log("Sorry, couldn\'t find this ID: ", inputId)
+    }
 }
 // getRowsDesired() is equivalent to getInputAsNumber("rows")
 // getColsDesired() is equivalent to getInputAsNumber("cols")
@@ -101,11 +49,3 @@ function createWithHtmlTable(rows, cols, highlight) {
     document.getElementById("output").innerHTML = string
 }
 
-
-console.log('highlighted value: ', getHighlightValue())
-
-timesTable(12)
-
-timesTable(20)
-
-timesTable('x')
